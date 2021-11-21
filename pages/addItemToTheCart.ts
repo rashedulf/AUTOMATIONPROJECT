e0b761c929db0054846136ddff2cfcaad9d5b22b
rashedul Faruq <rashedulf@gmail.com>
2021-11-21 15:27:05 -0500

    update.

--- pages/addItemToTheCart.ts
+++ pages/addItemToTheCart.ts
@@ -4,7 +4,6 @@ export class addItemToTheCart{
 
      selectProduct= element(by.css('a[href="http://automationpractice.com/index.php?id_product=7&controller=product"]'));
      addNumberofUnits= element(by.className('icon-plus'));
-     //selectSize= element(by.name('group_1')).$('[value="M"]');
      color= element(by.name('Green'));
      addToCart= element(by.name('Submit'));
     selectSizeElm= element(by.id('group_1'));
