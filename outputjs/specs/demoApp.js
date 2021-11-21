e0260fe0691bbdceb337f7abc8e6085994e29b7e
rashedul Faruq <rashedulf@gmail.com>
2021-11-21 03:27:46 -0500

    change to the test file.

--- outputjs/specs/demoApp.js
+++ outputjs/specs/demoApp.js
@@ -2,15 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const protractor_1 = require("protractor");
 describe("demoApp test", function () {
-    beforeEach(function () {
-        protractor_1.browser.get("https://juliemr.github.io/protractor-demo/");
-    });
     it("launch url check", function () {
-        expect(protractor_1.browser.getTitle()).toContain("Super");
-    });
-    it("launch another url check", function () {
-        (0, protractor_1.element)(protractor_1.by.model("first")).sendKeys("12");
-        (0, protractor_1.element)(protractor_1.by.model("second")).sendKeys("13");
-        (0, protractor_1.element)(protractor_1.by.id("gobutton")).click();
+        protractor_1.browser.waitForAngularEnabled(false);
+        protractor_1.browser.get("http://automationpractice.com/");
+        protractor_1.browser.sleep(3000);
     });
 });
+//it("launch another url check", function(){
+//element(by.model("first")).sendKeys("12");
+//element(by.model("second")).sendKeys("13");
+//element(by.id("gobutton")).click();
+// } )
+//})
