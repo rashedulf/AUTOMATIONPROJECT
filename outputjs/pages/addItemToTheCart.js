2c42837a89b582ecb866bde860db664747e1d3e9
rashedul Faruq <rashedulf@gmail.com>
2021-11-21 14:22:59 -0500

    Update.

--- outputjs/pages/addItemToTheCart.js
+++ outputjs/pages/addItemToTheCart.js
@@ -5,7 +5,7 @@ const protractor_1 = require("protractor");
 class addItemToTheCart {
     constructor() {
         this.selectProduct = (0, protractor_1.element)(protractor_1.by.css('a[href="http://automationpractice.com/index.php?id_product=7&controller=product"]'));
-        this.addNumberofUnits = (0, protractor_1.element)(protractor_1.by.class('icon-plus'));
+        this.addNumberofUnits = (0, protractor_1.element)(protractor_1.by.className('icon-plus'));
         //selectSize= element(by.name('group_1')).$('[value="M"]');
         this.color = (0, protractor_1.element)(protractor_1.by.name('Green'));
         this.addToCart = (0, protractor_1.element)(protractor_1.by.name('Submit'));
