be2c6d2d0be1d0cb768ed4d163ee3dd9de28fc81
rashedul Faruq <rashedulf@gmail.com>
2021-11-21 04:28:20 -0500

    update.

--- pages/searchProduct.ts
+++ pages/searchProduct.ts
@@ -3,11 +3,12 @@ import { browser, by, element } from "protractor";
 export class searchProducts{
 
      textBox= element(by.id('search_query_top'));
-     searchIcon= element(by.css('.btn btn-default.button-search'));
+     searchIcon= element(by.className('.btn btn-default.button-search'));
 
      typeProduct(){
          this.textBox.sendKeys("dress");
          this.searchIcon.click();
+         browser.sleep(30000);
      }
 
 }
