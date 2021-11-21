662f5158022b1bd14d190c27dc2557cb52c22458
rashedul Faruq <rashedulf@gmail.com>
2021-11-21 05:43:35 -0500

    checking files

--- outputjs/pages/addItemToTheCart.js
+++ outputjs/pages/addItemToTheCart.js
@@ -5,9 +5,18 @@ const protractor_1 = require("protractor");
 class addItemToTheCart {
     constructor() {
         this.selectProduct = (0, protractor_1.element)(protractor_1.by.css('a[href="http://automationpractice.com/index.php?id_product=7&controller=product"]'));
+        this.addNumberofUnits = (0, protractor_1.element)(protractor_1.by.class('.icon-plus'));
+        this.selectSize = (0, protractor_1.element)(protractor_1.by.id('group_1')).$('[value="M"]');
+        this.color = (0, protractor_1.element)(protractor_1.by.name('Green'));
+        this.addToCart = (0, protractor_1.element)(protractor_1.by.name('Submit'));
     }
     pickTheProduct() {
         this.selectProduct.click();
+        this.addNumberofUnits.click();
+        this.selectSize.click();
+        protractor_1.browser.sleep(20000);
+        this.color.click();
+        this.addToCart.click();
     }
 }
 exports.addItemToTheCart = addItemToTheCart;
--- pages/addItemToTheCart.ts
+++ pages/addItemToTheCart.ts
@@ -3,7 +3,7 @@ import { browser, by, element } from "protractor";
 export class addItemToTheCart{
 
      selectProduct= element(by.css('a[href="http://automationpractice.com/index.php?id_product=7&controller=product"]'));
-     addNumberofUnits= element(by.class('.icon-plus'));
+     //addNumberofUnits= element(by.class('.icon-plus'));
      selectSize= element(by.id('group_1')).$('[value="M"]');
      color= element(by.name('Green'));
      addToCart= element(by.name('Submit'));
@@ -11,7 +11,7 @@ export class addItemToTheCart{
      
      pickTheProduct(){
          this.selectProduct.click();
-         this.addNumberofUnits.click();
+         //this.addNumberofUnits.click();
          this.selectSize.click();
          browser.sleep(20000);
          this.color.click();
