811cd441eb96caab1de97984ac8d6650e2b08423
rashedul Faruq <rashedulf@gmail.com>
2021-11-21 06:01:05 -0500

    update

--- outputjs/pages/addItemToTheCart.js
+++ outputjs/pages/addItemToTheCart.js
@@ -6,7 +6,7 @@ class addItemToTheCart {
     constructor() {
         this.selectProduct = (0, protractor_1.element)(protractor_1.by.css('a[href="http://automationpractice.com/index.php?id_product=7&controller=product"]'));
         //addNumberofUnits= element(by.class('.icon-plus'));
-        this.selectSize = (0, protractor_1.element)(protractor_1.by.id('group_1')).$('[value="M"]');
+        this.selectSize = (0, protractor_1.element)(protractor_1.by.id('uniform-group_1')).$('[value="M"]');
     }
     //color= element(by.name('Green'));
     //addToCart= element(by.name('Submit'));
