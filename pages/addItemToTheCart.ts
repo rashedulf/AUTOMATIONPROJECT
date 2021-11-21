fec57cd41baeb534e8c9929ea6846308c837f60c
rashedul Faruq <rashedulf@gmail.com>
2021-11-21 14:27:23 -0500

    Update.

--- pages/addItemToTheCart.ts
+++ pages/addItemToTheCart.ts
@@ -3,7 +3,7 @@ import { browser, by, element } from "protractor";
 export class addItemToTheCart{
 
      selectProduct= element(by.css('a[href="http://automationpractice.com/index.php?id_product=7&controller=product"]'));
-     addNumberofUnits= element(by.class('icon-plus'));
+     addNumberofUnits= element(by.className('icon-plus'));
      //selectSize= element(by.name('group_1')).$('[value="M"]');
      color= element(by.name('Green'));
      addToCart= element(by.name('Submit'));
