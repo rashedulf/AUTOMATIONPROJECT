82a65af2eb5da98467f30d1d0ae40989cdbb7625
rashedul Faruq <rashedulf@gmail.com>
2021-11-21 03:25:30 -0500

    Change in Test file.

--- specs/demoApp.ts
+++ specs/demoApp.ts
@@ -2,21 +2,22 @@ import { browser, by, element } from "protractor";
 
 describe("demoApp test", function(){
 
-    beforeEach(function(){
-        browser.get("https://juliemr.github.io/protractor-demo/");
+    it("launch url check", function(){
+        browser.waitForAngularEnabled(false);
+        browser.get("http://automationpractice.com/");
+        browser.sleep(3000);
 
     })
 
-    it("launch url check", function(){
-        expect(browser.getTitle()).toContain("Super")
+    
 
     } )
 
-    it("launch another url check", function(){
-        element(by.model("first")).sendKeys("12");
-        element(by.model("second")).sendKeys("13");
-        element(by.id("gobutton")).click();
+    //it("launch another url check", function(){
+       //element(by.model("first")).sendKeys("12");
+        //element(by.model("second")).sendKeys("13");
+        //element(by.id("gobutton")).click();
 
         
-    } )
-})
+   // } )
+//})
