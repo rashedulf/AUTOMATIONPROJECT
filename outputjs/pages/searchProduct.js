b1f4e955ee80215ae3504205f805280a37232a9c
rashedul Faruq <rashedulf@gmail.com>
2021-11-21 04:36:52 -0500

    update.

--- outputjs/pages/searchProduct.js
+++ outputjs/pages/searchProduct.js
@@ -5,7 +5,7 @@ const protractor_1 = require("protractor");
 class searchProducts {
     constructor() {
         this.textBox = (0, protractor_1.element)(protractor_1.by.id('search_query_top'));
-        this.searchIcon = (0, protractor_1.element)(protractor_1.by.className('.btn btn-default.button-search'));
+        this.searchIcon = (0, protractor_1.element)(protractor_1.by.name('submit_search'));
     }
     typeProduct() {
         this.textBox.sendKeys("dress");
